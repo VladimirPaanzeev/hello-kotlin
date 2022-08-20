f223d4b70ef22ef8add3fd18107eb7daf63613ef
Vladimir Palanzeev <palanzeev.vova@yandex.ru>
2022-08-21 01:47:47 +0500

    add module2

---
+++ src/main/kotlin/module2/lesson1.kt
@@ -0,0 +1,10 @@
+package module2
+
+fun main() {
+    println("Hello World")
+    println()  // Одноыстроччный коментарий
+    print("Hello ")
+    print("Kotlin")
+   /* многострочный
+    комннтарий */
+}
