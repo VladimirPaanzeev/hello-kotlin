c566b268453a1ae68ef9c2613663752ef756d801
Vladimir Palanzeev <palanzeev.vova@yandex.ru>
2022-08-20 18:14:22 +0500

    upgrad main, add lesson1

---
+++ src/main/kotlin/module1/lesson1.kt
@@ -0,0 +1,13 @@
+package module1
+
+fun main() {
+    val hello = "Hello"
+    val kotlin = "kotlin"
+
+    printHello(hello, kotlin)
+}
+
+fun printHello(a: String, b: String){
+    println("$a $b!")
+
+}
