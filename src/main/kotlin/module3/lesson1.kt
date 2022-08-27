d8e789594d8d94294947000431d5998438b562d8
Vladimir Palanzeev <palanzeev.vova@yandex.ru>
2022-08-28 00:20:50 +0500

    объединение отложенных изменений и последнего комита

--- src/main/kotlin/module3/lesson1.kt
+++ src/main/kotlin/module3/lesson1.kt
@@ -2,8 +2,12 @@ package module3
 
 fun main() {
     val firstVariable = 6
+<<<<<<< Updated upstream
     val secondVariable = 10
     secondVariable += firstVariable
+=======
+    var secondVariable = 10
+>>>>>>> Stashed changes
     println("Сумма чисел $firstVariable и $secondVariable равна: ${sum(firstVariable, secondVariable)}")
 }
 
