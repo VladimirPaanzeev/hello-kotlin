9b4ebe4e1120eed01ca51548c3b020d4dda7e263
Vladimir Palanzeev <palanzeev.vova@yandex.ru>
2022-08-27 23:41:49 +0500

    Удалил лишний коментарий и лишнюю переменную

--- src/main/kotlin/module3/lesson1.kt
+++ src/main/kotlin/module3/lesson1.kt
@@ -1,15 +1,10 @@
 package module3
 
 fun main() {
-    val number: Int = 5
-
     val firstVariable = 6
     var secondVariable = 10
     secondVariable += firstVariable
-    println("Сумма чисел $firstVariable и $secondVariable равна: ${Sum(firstVariable, secondVariable)}")
+    println("Сумма чисел $firstVariable и $secondVariable равна: ${sum(firstVariable, secondVariable)}")
 }
 
-fun Sum(number1: Int, number2: Int) = number1 + number2
-
-
-//5.04
+fun sum(number1: Int, number2: Int) = number1 + number2
