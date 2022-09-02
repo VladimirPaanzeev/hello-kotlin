6e6815a3f656827d844dad6cbfe29dd0c49562d0
Vladimir Palanzeev <palanzeev.vova@yandex.ru>
2022-09-02 06:15:59 +0500

    lesson 1

--- src/main/kotlin/module7/lesson1/Car.kt
+++ src/main/kotlin/module7/lesson1/Car.kt
@@ -1,8 +1,22 @@
 package module7.lesson1
 
+import kotlin.random.Random
+
 class Car {
     val brand = "Audi"
     val model = "A3"
     val color = "Red"
 
+    fun move (){
+        val distance = Random.nextInt(1, 10)
+        println("Автомобиль проехал $distance км")
+    }
+
+    fun start() {
+        println(" Автомобиль запустился")
+    }
+    
+    fun stop() {
+        println(" Автомобиль остановился")
+    }
 }
--- src/main/kotlin/module7/lesson1/main.kt
+++ src/main/kotlin/module7/lesson1/main.kt
@@ -1,2 +1,14 @@
 package module7.lesson1
 
+fun main() {
+    val auto = Car()
+
+    println("Информация по автомобилю")
+    println("${auto.brand} ${auto.model} ${auto.color}")
+
+    auto.start()
+    auto.move()
+    auto.move()
+    auto.move()
+    auto.stop()
+}
