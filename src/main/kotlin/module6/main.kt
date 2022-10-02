bc7809b3b8e46389c1632693c8f11d12e5cbefe3
Vladimir Palanzeev <palanzeev.vova@yandex.ru>
2022-10-02 08:23:58 +0500

    ***

--- src/main/kotlin/module6/main.kt
+++ src/main/kotlin/module6/main.kt
@@ -1,2 +1,10 @@
 package module6
 
+fun main() {
+    val list = listOf<String>("Один", "Два", "Три")
+    val map = mutableMapOf<Int, String>()
+
+    list.forEach { item -> println(item) }
+    list.forEachIndexed { index, i -> map[index+1] = i }
+    println(map)
+}
